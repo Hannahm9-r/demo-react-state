15ed4210d8ec4415492fe72870776189111b5af6
Hannah Malik <hannah99malik@gmail.com>
2021-01-14 01:24:44 +0500

    Update to Assignment

--- Room.js
+++ Room.js
@@ -5,19 +5,50 @@ function Room() {
   let [isLit, setLit] = useState (false);
   let [age, setAge] = useState (24);
 
+  //Assignment temp , on , and off (buttons)
+  let [temp, setTemp] = useState (22);
+
  
   return (
-    <div className = {`room ${(isLit? "lit" : "dark")}`}>
+    <div className = {`room ${(isLit? "lit" : "dark")}`}> 
       This room is {isLit ? "Lit" : "Dark"}
       <br/>
       Age : {age}
       <br/>
+      The Room Temperature is: <quote><b>{temp}</b><sup>o</sup>C</quote>
+
+
+      <button onClick = {() => {
+          console.log ('Inc temp arrow func')
+          setTemp(++temp)}}>+
+          </button>
+
+
+      <button onClick = {() => {
+          console.log ('Dec temp arrow func')
+          setTemp(--temp)}}>-
+          </button>
+
+
+      <br/>
       <button onClick = {()=> setLit(!isLit)} >Toggle Light</button>
+
+
+
       <br/>
       <button onClick = { ()=> {
           console.log ("Increase Age arrow function");
           setAge(++age);}
           } >increase Age</button>
+
+          <div>
+              <button onClick = {() => setLit(isLit? isLit : !isLit)}>ON</button>
+
+              
+              <button onClick = {() => setLit(isLit? !isLit : isLit)}>OFF</button>
+          
+          </div>  
+    
     </div>
   );
 }
@@ -44,12 +75,8 @@ export default Room;
 
 
 
-
-
-
-
-
-
+// These functions were changed to arrow functions.
+//                ^
  /*function updateLit () {
       console.log ("Button clicked");
       setLit(!isLit);
